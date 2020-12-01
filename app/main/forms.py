e54209d41a8bda090fdcef08d5c6a8715a3a6e71
uwase-diane <diane.uwase13@gmail.com>
2020-12-01 05:01:10 -0500

    updateprofile class

--- app/main/forms.py
+++ app/main/forms.py
@@ -1,8 +1,21 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,PasswordField,SubmitField, TextAreaField,ValidationError,RadioField
+from wtforms import StringField,PasswordField,SubmitField,SelectField, TextAreaField,ValidationError,RadioField
 from wtforms.validators import Required,Email,EqualTo
-from ..models import User
+
 
 class UpdateProfile(FlaskForm):
-    bio = TextAreaField('Tell us about you.',validators = [Required()])
-    submit = SubmitField('Submit')
+    bio=TextAreaField('write a brief bio about you.', validators=[Required()])
+    submit=SubmitField('save')
+
+
+class PitchForm(FlaskForm):
+    title=StringField('Title', validators=[Required()])
+    category=SelectField('Category',choices=[ ('education','education'), ('music','music'),('religion','religion'),], validators=[Required()])
+    post=TextAreaField('Your Pitch.', validators=[Required()])
+    submit=SubmitField('Pitch')
+
+class CommentForm(FlaskForm):
+    comment=TextAreaField('Leave a comment',validators=[Required()])
+    submit=SubmitField('Comment')
+       
+  
